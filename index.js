e8c25793748a6a7d0cb0f35aa70f078dd13a8b2d
Anjaneya <anjaneya.singh94@gmail.com>
2021-12-17 09:44:26 +0530

    prod website naming fix

--- index.js
+++ index.js
@@ -37,7 +37,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("website/build"));
 
   const path = require("path");
-  res.sendFile(path.resolve(__dirname, "website", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "website", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
