4869a4733401c347a669a0f1d30e8437a872ba2d
Anjaneya <anjaneya.singh94@gmail.com>
2021-12-17 09:36:59 +0530

    prod naming fix

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("website/build"));
 
   const path = require("path");
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "website", "build", "index.html"));
 }
 
 const PORT = process.env.PORT || 5000;
